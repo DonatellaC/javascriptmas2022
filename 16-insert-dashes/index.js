ede4d6d8ddac4f0dbbbf81ca91183995c349502f
DonatellaC <donatella.carta@mailbox.org>
2022-12-21 10:00:36 +0100

    added starter files day 16

---
+++ 16-insert-dashes/index.js
@@ -0,0 +1,16 @@
+// Instructions:
+// Transform a given sentence into a new one with dashes between each two consecutive letters.
+
+// Example
+
+// For inputString = "aca caba", the output should be insertDashes(inputString) = "a-c-a c-a-b-a".
+
+// Hint
+
+// join()
+// split()
+
+function insertDashes(arr) {
+  // write code here
+}
+console.log(insertDashes("a-b-a c-a-b-a"));
