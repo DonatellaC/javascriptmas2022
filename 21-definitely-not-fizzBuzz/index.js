76565f8cb5cbfd21ddfdda1bde916c4d43365b42
DonatellaC <donatella.carta@mailbox.org>
2022-12-21 16:29:53 +0100

    added solution day 21

--- 21-definitely-not-fizzBuzz/index.js
+++ 21-definitely-not-fizzBuzz/index.js
@@ -17,8 +17,23 @@ Your function's output should look something like this:
 3 - Vacation! 
 4 - :(
 5 - $100,000 bonus!
+
+Hint: Remainder operator, modulo 
  */
 
-function awardBonuses() {}
+function awardBonuses() {
+  let i;
+  for (i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log(`${i} - JACKPOT! 1 Million and a Yacht!`);
+    } else if (i % 3 === 0) {
+      console.log(`${i} - Vacation!`);
+    } else if (i % 3 === 0) {
+      console.log(`${i} - $100,000 bonus!`);
+    } else {
+      console.log(`${i} - :(`);
+    }
+  }
+}
 
 awardBonuses();
