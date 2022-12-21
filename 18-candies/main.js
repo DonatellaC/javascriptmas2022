360eec7dbbe62a70dfb5f74b1e2d2bf73e14b886
DonatellaC <donatella.carta@mailbox.org>
2022-12-21 10:49:29 +0100

    added solution day 18

--- 18-candies/main.js
+++ 18-candies/main.js
@@ -1,9 +1,10 @@
 /* Instructions:
 
-n children have got m pieces of candy. They want to eat as much candy
-as they can, but each child must eat exactly the same amount of candy 
-as any other child. Determine how many pieces of candy will be eaten
-by all the children together. Individual pieces if candy cannot be split.
+n children have got m pieces of candy. They want to eat as much candy as they can, 
+but each child must eat exactly the same amount of candy as any other child. 
+
+Determine how many pieces of candy will be eaten by all the children together. 
+Individual pieces if candy cannot be split.
 
 Example
 
@@ -18,7 +19,7 @@ Math.floor()
 */
 
 function candies(children, candy) {
-  //  write code here.
+  return Math.floor(candy / children) * children;
 }
 
 console.log(candies(3, 10));
